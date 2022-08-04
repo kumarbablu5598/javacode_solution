28b14d75d1941d1a567668cc9075c61d2f76c9d7
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2022-08-04 16:09:23 +0530

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,53 @@
+
+
+Take Integer N as input and print the following pattern.
+    
+I/p  N=5
+
+O/P
+
+      *
+     **
+    ***
+   ****
+  *****
+
+I/P N=7
+    
+O/p   
+
+      *
+     **
+    ***
+   ****
+  *****
+ ******
+*******   
+
+// java code
+
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            
+            for(int j=0;j<n;j++)
+            {  
+                if(j>=n-1-i)
+                System.out.print("*");
+                else System.out.print(" ");
+            }
+            System.out.println();
+             
+        }
+    }
+}
+
