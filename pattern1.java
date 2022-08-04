c5cd7e75d262638c691d4cdbd2f987e40cde8e2b
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2022-08-04 15:28:09 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,38 @@
+/*
+
+I/p  N=5
+
+O/P
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+*/
+
+// java code
+
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+             
+        }
+    }
+}
+
